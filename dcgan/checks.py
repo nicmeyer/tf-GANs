2afe12e173704c656028819bbb49f2231c11e2c1
nicholas <nicholasmeyer92@gmail.com>
2023-02-27 23:23:46 +0100

    Add test for strides and padding.

--- dcgan/checks.py
+++ dcgan/checks.py
@@ -4,6 +4,8 @@ import torch
 
 # Check how to map Keras implementation of Conv2DTranspose to Torch
 # implementation of ConvTranspose2d.
+
+# Test without strides.
 r = np.random.normal(size=(20, 2, 50, 100))
 
 tf_input = tf.convert_to_tensor(r)
@@ -18,6 +20,25 @@ tf_result = tf_conv(tf_input).numpy()
 
 torch_input = torch.Tensor(r)
 torch_conv = torch.nn.ConvTranspose2d(2, 3, 4, bias=False)
+torch_conv.weight.data.fill_(1.)
+torch_result = torch_conv(torch_input).detach().numpy()
+
+assert tf_result.shape == torch_result.shape
+assert np.isclose(tf_result, torch_result, atol=1e-05).all()
+
+# Test with strides and padding.
+tf_conv = tf.keras.layers.Conv2DTranspose(
+  3,
+  4,
+  (2, 2),
+  padding="same",
+  data_format="channels_first",
+  kernel_initializer=tf.keras.initializers.Ones(),
+  use_bias=False
+)
+tf_result = tf_conv(tf_input).numpy()
+
+torch_conv = torch.nn.ConvTranspose2d(2, 3, 4, 2, 1, bias=False)
 torch_conv.weight.data.fill_(1.)
 torch_result = torch_conv(torch_input).detach().numpy()
 
