4d31d24271c2cee6a74e6fc023c20b80adf309ce
nicmeyer <74037918+nicmeyer@users.noreply.github.com>
2023-05-25 15:49:41 +0200

    Fix test.

--- gan/gan_test.py
+++ gan/gan_test.py
@@ -15,18 +15,18 @@ class ScheduleTest(tf.test.TestCase):
     init_learning_rate = tf.random.uniform([])
     decay_factor = tf.random.uniform([], 2, 4)
     min_lr = tf.maximum(
-      x=self.init_learning_rate - tf.random.uniform([]),
+      x=init_learning_rate - tf.random.uniform([]),
       y=0,
     )
     bound = tf.math.ceil(
       tf.math.log(
-        self.init_learning_rate / self.min_lr) / tf.math.log(self.decay_factor))
+        init_learning_rate / min_lr) / tf.math.log(decay_factor))
     bound = tf.cast(bound, tf.int32) + 1
     lr = repeat_function_call(
       func=lambda x: schedule(x, init_learning_rate, decay_factor, min_lr),
       n=bound,
     )
-    self.assertEqual(lr, self.min_lr)
+    self.assertEqual(lr, min_lr)
 
 
 if __name__ == '__main__':
